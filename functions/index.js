6c4a236eb56617c8fedfb5925b8e7d3a4687c75c
Benjamin Kowalski <Benjamin.Kowalski.1987@gmail.com>
2019-09-14 17:46:35 +0200

    👨‍💻 Delete more firebase config stuff

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-
-const { newUserTrigger } = require('./user.new.js')
-
-admin.initializeApp()
-
-exports.newUserTrigger = newUserTrigger
