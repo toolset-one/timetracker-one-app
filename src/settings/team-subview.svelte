65c10523f7c2f27894758ca50213ac88d4a72fd9
Benjamin Kowalski <Benjamin.Kowalski.1987@gmail.com>
2019-10-02 16:35:26 +0200

    👨‍💻 Hide invite button for non-admins

--- src/settings/team-subview.svelte
+++ src/settings/team-subview.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Page from 'page'
 	import { onMount } from 'svelte';
-	import { authSignOut } from '../stores/auth-store.js'
+	import { authStore, authSignOut } from '../stores/auth-store.js'
 	import { uiStore } from '../stores/ui-store.js'
 	import { teamStore, teamStoreChangeTitle } from '../stores/team-store.js'
 
@@ -54,9 +54,11 @@
 					Team &middot; {$teamStore.active.title}
 				</h2>
 			</div>
-			<div class="add-button-wrapper">
-				<UiButton label="Invite Member" on:click={e => openOverlayComponent({ component: 'invite', id: null })} />
-			</div>
+			{#if $authStore.user && $teamStore.active && $authStore.user.teams[$teamStore.active.id] === 'ADMIN'}
+				<div class="add-button-wrapper">
+					<UiButton label="Invite Member" on:click={e => openOverlayComponent({ component: 'invite', id: null })} />
+				</div>
+			{/if}
 		</header>
 
 
@@ -69,18 +71,20 @@
 			{/each}
 		</ul>
 
-		{#if $teamStore.active.invitations.length > 0}
-			<h3>
-				Invitations to the team
-			</h3>
-			<ul class="entries">
-				{#each $teamStore.active.invitations as invitation, i}
-					<TeamEntry data={invitation}
-						invitation={true}
-						first={i == 0}
-						last={i == $teamStore.active.invitations.length - 1} />
-				{/each}
-			</ul>
+		{#if $authStore.user && $teamStore.active && $authStore.user.teams[$teamStore.active.id] === 'ADMIN'}
+			{#if $teamStore.active.invitations.length > 0}
+				<h3>
+					Invitations to the team
+				</h3>
+				<ul class="entries">
+					{#each $teamStore.active.invitations as invitation, i}
+						<TeamEntry data={invitation}
+							invitation={true}
+							first={i == 0}
+							last={i == $teamStore.active.invitations.length - 1} />
+					{/each}
+				</ul>
+			{/if}
 		{/if}
 	{/if}
 
