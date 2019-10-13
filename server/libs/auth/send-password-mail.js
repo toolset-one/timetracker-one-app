0ff14374868ec87fba67ebb7faa028764ce53a95
Benjamin Kowalski <Benjamin.Kowalski.1987@gmail.com>
2019-10-13 15:27:24 +0200

    🧨 Delete server code from frontend repository

--- server/libs/auth/send-password-mail.js
+++
@@ -1,7 +0,0 @@
-const { db } = require('../../libs/db.js'),
-	{ sendMail } = require('../../libs/mail.js')
-
-exports.sendPasswordMail = async (ws, sockets, { promiseId, email }) =>
-	new Promise(async (resolve, reject) => {
-		resolve(true)
-	})
